674f4e0379a17a45d2734bae78f62b0714d616f3
sreejith <sreejith001v@gmail.com>
2022-12-16 16:14:19 +0530

    usestate

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react'
+import React from 'react';
+
 
 function App() {
+    const [count,setCount]=useState(0)
+   // const [sqre,setSqre]=useState(0)
+
+    const addCount=()=>{
+        setCount(count+1)
+        console.log(count);
+    }
+    const decreaseCount=()=>{
+        setCount(count-1)
+        console.log(count);
+    }
+    // const square=()=>{
+    //   setSqre(count*count)
+    //     console.log(count);
+    // }
+    const reset=()=>{
+      //setSqre(0)
+      setCount(0)
+        
+    }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div >
+      <h1>Counter:{count}</h1>
+      {/* <h2>square:{sqre}</h2> */}
+        <button onClick={addCount}>Increment</button>
+        <button onClick={decreaseCount}>Decrement</button>
+        {/* <button onClick={square}>square</button> */}
+        <button onClick={reset}>Reset</button>
+        
     </div>
   );
 }
